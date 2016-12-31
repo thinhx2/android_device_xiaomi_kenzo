dedacd74f2ecb379773f4a8e79d35aa8d20fc17c
minz1 <minz1@github.com>
2016-12-31 07:33:17 -0500

    flo071: Add Vulkan

--- device.mk
+++ device.mk
@@ -56,3 +56,8 @@ $(call inherit-product, device/xiaomi/msm8956-common/msm8956.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+# Vulkan Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
